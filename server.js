b0fa98407433e32520aaf4f266de92fd3917e08e
Alex <alexandre.vernet99@gmail.com>
2022-12-01 17:28:19 +0100

    wip: Implement SSR

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require('cors');
 const port = 3333;
 
+const path = require("path");
+const fs = require("fs");
+
 const buildFolderName = 'build';
 app.use(express.json());
 app.use(cors());
@@ -16,6 +19,20 @@ const corsOptions = {
 
 app.get('/', async (req, res) => {
     res.sendFile(`${ __dirname }/${ buildFolderName }/index.html`);
+
+    // const app = ReactDOMServer.renderToString(<App />);
+    // const indexFile = path.resolve('./build/index.html');
+    //
+    // fs.readFile(indexFile, 'utf8', (err, data) => {
+    //     if (err) {
+    //         console.error('Something went wrong:', err);
+    //         return res.status(500).send('Oops, better luck next time!');
+    //     }
+    //
+    //     return res.send(
+    //         data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+    //     );
+    // });
 });
 
 app.get('/test', cors(corsOptions), (req, res) => {
